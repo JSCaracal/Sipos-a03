55b896cd2eba14acbcbfbc6727f212a6166ca82f
JSCaracal <siposj@knights.ucf.edu>
2021-09-23 18:29:43 -0400

    E24 commit, psuedo code and indentifiers only

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,22 @@
+package baseline;
+
+public class Solution24 {
+    boolean isAnagram(String wordOne, String wordTwo){
+
+        //If wordOne isn't the same length as wordTwo, the function returns false
+        //For each loop will itterate through wordOne
+        //Use the contains method to see if wordTwo contains the the letter itterating through
+        //If wordTwo Doesn't contain the letter in word1, return false
+        //Return true if all cases aren't false
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String f1;
+        String f2;
+        //Prompt user for first string
+        //prompt for second
+        //Store strings, and call the isAnagram method
+        //Output the results.
+    }
+}
