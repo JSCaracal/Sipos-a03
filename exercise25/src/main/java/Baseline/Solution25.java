bfc302b98ddf3b0dcc1ec82f2cc86a39b605226e
JSCaracal <siposj@knights.ucf.edu>
2021-09-24 17:31:25 -0400

    E25 Psuedocode and design

---
+++ exercise25/src/main/java/Baseline/Solution25.java
@@ -0,0 +1,38 @@
+package Baseline;
+
+import java.util.Scanner;
+
+public class Solution25 {
+    //Create a password via a string
+    String passInput(){
+        String in;
+        return in;
+    }
+    //checks to see if its just numbers
+    boolean hasNumbers(String pass){
+        return true;
+    }
+    //checks to see if its just letters
+    boolean hasLetters(String pass){
+        return true;
+    }
+    //checks to see if it incoprperates numbers and letters
+    boolean hasNumsAndLetters(String pass){
+        return true;
+    }
+    //checks to see if it incrops speccial characters
+    boolean specialCharacters(String pass){
+        return true;
+    }
+
+    //return a statement that informs the user of said password
+    int passwordValidator(String pass){
+        return 0;
+    }
+    public static void main(String[] args) {
+        String password;
+        //Retieve Password
+        //call the password validator function
+        //print results
+    }
+}
