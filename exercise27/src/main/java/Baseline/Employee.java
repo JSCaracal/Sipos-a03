5830b6e446f426842ef96b17ba0c127f6d6f7af5
JSCaracal <siposj@knights.ucf.edu>
2021-09-28 19:52:41 -0400

    Initial Design01 of 27

---
+++ exercise27/src/main/java/Baseline/Employee.java
@@ -0,0 +1,23 @@
+/*
+ *
+ *  *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  *  Copyright 2021 Joshua Sipos
+ *
+ */
+
+package Baseline;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private String id;
+    private int zipCode;
+
+    public Employee(String fName, String lName, String id, int zCode){
+            this.firstName = fName;
+            this.lastName = lName;
+            this.id = id;
+            this.zipCode = zCode;
+    }
+    //Create a
+}
---
+++ exercise27/src/main/java/Baseline/Solution27.java
@@ -0,0 +1,26 @@
+/*
+ *
+ *  *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  *  Copyright 2021 Joshua Sipos
+ *
+ */
+/*
+
+Write a program that prompts for a first name, last name, employee ID, and ZIP code.
+Ensure that the input is valid according to these rules:
+The first name must be filled in.
+The last name must be filled in.
+The first and last names must be at least two characters long.
+An employee ID is in the format AA-1234. So, two letters, a hyphen, and four numbers.
+The ZIP code must be a number.
+
+
+ */
+
+package Baseline;
+
+public class Solution27 {
+    //Create a Class the contains the fields
+    //With in that class attempt exception handling if invalid credentials are entered
+
+}
