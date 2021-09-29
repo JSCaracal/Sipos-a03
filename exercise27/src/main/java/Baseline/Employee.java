2525316b19911fd23d17ec166c63391fb3afb600
JSCaracal <siposj@knights.ucf.edu>
2021-09-29 11:50:16 -0400

    Adding solution 27 and its class file, tests not included in this commit

--- exercise27/src/main/java/Baseline/Employee.java
+++ exercise27/src/main/java/Baseline/Employee.java
@@ -7,11 +7,14 @@
 
 package Baseline;
 
+import java.util.Scanner;
+
 public class Employee {
     private String firstName;
     private String lastName;
     private String id;
     private int zipCode;
+    ;
 
     public Employee(String fName, String lName, String id, int zCode){
             this.firstName = fName;
@@ -19,5 +22,78 @@ public class Employee {
             this.id = id;
             this.zipCode = zCode;
     }
-    //Create a
+    //Create sub functions that validate the program
+    public boolean nameValidator(String name){
+        if(name.length() < 2){
+            return false;
+        }
+        return true;
+    }
+    public boolean zipValidator(){
+        String zip = Integer.toString(zipCode);
+        if(zip.length() < 5 || zip.length() > 5){
+            return false;
+        }
+        return true;
+    }
+    public boolean idValidator(){
+        //Is it the id the length of 2 letters,1 hyphen, and 4 nums?
+        if(id.length() != 7){
+            return false;
+        }
+        //Go char by char
+        for(int i = 0; i < id.length(); i++){
+            //Check the first 2
+            if(i < 2){
+                if(id.charAt(i) >= 65 && id.charAt(i) <= 90 || id.charAt(i) >= 91 && id.charAt(i) <= 122){
+                    continue;
+                }
+                else{
+                    return false;
+                }
+            }
+            //Checks for Hyphon
+            if(i == 2){
+                if(id.charAt(i) != 45){
+                    return false;
+                }
+                else{
+                    continue;
+                }
+            }
+            //Check for the last 4 numbs
+            if(i > 2) {
+                if (id.charAt(i) >= 48 && id.charAt(i) <= 57) {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
+    //Create id validator
+    void validateInput(Employee employee){
+        boolean valid = true;
+        if(!employee.nameValidator(employee.firstName)){
+            System.out.printf("The first name must be 2 characters long\n");
+            valid = false;
+        }
+        if(!employee.nameValidator(employee.lastName)){
+            System.out.printf("The last name must be 2 characters long\n");
+            valid = false;
+        }
+        if(!employee.idValidator()){
+            System.out.printf("The id must be in the format of: AA-1234\n");
+            valid = false;
+        }
+        if(!zipValidator()){
+            System.out.println("The Zip code must be a 5 digit number");
+            valid = false;
+        }
+        if(valid == true){
+            System.out.println("There were no errors found");
+        }
+    }
 }
--- exercise27/src/main/java/Baseline/Solution27.java
+++ exercise27/src/main/java/Baseline/Solution27.java
@@ -19,8 +19,30 @@ The ZIP code must be a number.
 
 package Baseline;
 
+import java.util.Scanner;
+
 public class Solution27 {
+    private static final Scanner input = new Scanner(System.in);
     //Create a Class the contains the fields
     //With in that class attempt exception handling if invalid credentials are entered
+    //Get credentials using an input method
+    public Employee inputEmployee(){
+        System.out.printf("Please enter your first name: ");
+        String fName = input.next();
+        System.out.printf("Please enter your last name: ");
+        String lName = input.next();
+        System.out.printf("Please enter your id: ");
+        String id = input.next();
+        System.out.printf("Please enter your zip code: ");
+        int zip = input.nextInt();
+        Employee out = new Employee(fName,lName,id,zip);
+        return out;
+
+    }
+    public static void main(String[] args) {
+        Solution27 s27 = new Solution27();
+        Employee emp = s27.inputEmployee();
+        emp.validateInput(emp);
+    }
 
 }
