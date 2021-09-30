b68334259cc77fe716040d346606b6198c26f4ef
JSCaracal <siposj@knights.ucf.edu>
2021-09-30 01:00:39 -0400

    Exercise 29, tests and code

---
+++ exercise29/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+Z
+*src/test/java/Baseline/Solution29Test.java,8\b\8b7c019b4c45cf8c057effdd19ee77cade07a399
+V
+&src/main/java/Baseline/Solution29.java,2\b\2b412b25cb738bb3a046a178a4edcacc288acc93
---
+++ exercise29/src/main/java/Baseline/Solution29.java
@@ -0,0 +1,54 @@
+
+/*
+ *
+ *  *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  *  Copyright 2021 Josua Sipos
+ *
+ */
+
+package Baseline;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Solution29 {
+    //Create an input function per variable that loops until the input matches requirements
+    private static final Scanner input = new Scanner(System.in);
+
+    //Rate of return
+    int inputRate() {
+        int rate = 0;
+        boolean isValid = false;
+        //Do basic true loop
+        do{
+            System.out.printf("Enter the input rate: ");
+            try{
+                rate = input.nextInt();
+                if(rate == 0){
+                    System.out.println("Please enter a numerical value above zero");
+                    continue;
+                }
+                isValid = true;
+            }catch (InputMismatchException e){
+                System.out.println("Please enter a numerical value");
+                input.next();
+            }
+
+        }while (!isValid);
+        return rate;
+    }
+
+    void returnRateCalculation(int rate){
+        System.out.printf("It will take %d years to double your initial investment",72/rate);
+    }
+
+
+
+        public static void main (String[]args){
+            Solution29 s29 = new Solution29();
+            int rate = s29.inputRate();
+            s29.returnRateCalculation(rate);
+
+        }
+
+    }
