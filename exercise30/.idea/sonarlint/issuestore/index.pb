00633abf6eaa3bd1cbc51979e0afa970ca649163
JSCaracal <siposj@knights.ucf.edu>
2021-09-30 13:16:21 -0400

    Exercise 30, no tests are implemented

---
+++ exercise30/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+V
+&src/main/java/Baseline/Solution30.java,2\1\2130bc58d3228eefb9192e268a1996cde131b2fb
---
+++ exercise30/src/main/java/Baseline/Solution30.java
@@ -0,0 +1,31 @@
+package Baseline;
+
+public class Solution30 {
+    //Create a function that handles the table
+    void printMultiplicationTable(){
+        //for loop that includes 12
+        int product;
+        for(int i = 1; i <= 12; i++){
+            //Make another loop the represents collums, multiply by the first number
+            for(int j = 1; j <=12;j++){
+                product = i*j;
+                if(product < 10){
+                    System.out.printf("%d   ",product);
+                }
+                else if(product < 100 && product >= 10) {
+                    System.out.printf("%d  ",product);
+                }
+                else {
+                    System.out.printf("%d ",product);
+                }
+            }
+            System.out.println("");
+        }
+    }
+    public static void main(String[] args) {
+        Solution30 s30 = new Solution30();
+        s30.printMultiplicationTable();
+    }
+}
+
+
