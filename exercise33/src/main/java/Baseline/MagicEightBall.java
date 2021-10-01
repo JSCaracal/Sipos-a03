67d2590f3f911d7f961fb15b00237ec2221152e2
JSCaracal <siposj@knights.ucf.edu>
2021-10-01 16:10:43 -0400

    Psuedo code and initial Design

---
+++ exercise33/src/main/java/Baseline/MagicEightBall.java
@@ -0,0 +1,15 @@
+package Baseline.Baseline;
+import java.util.Random;
+public class MagicEightBall {
+    Random rand = new Random();
+    //No args Constructor which initilized variable
+    private int choice;
+    public MagicEightBall(){
+            this.choice = 0;
+    }
+    //Method that selects a random number and prints out the statement from a list.
+    public void shake(){
+
+    }
+
+}
---
+++ exercise33/src/main/java/Baseline/Solution33.java
@@ -0,0 +1,17 @@
+package Baseline;
+
+public class Solution33 {
+    /*
+    Magic 8 ball
+    Arrays are great for storing possible responses from a program.
+    If you combine that with a random number generator, you can pick a random entry from this list, which works great
+    for games.
+    Create a Magic 8 Ball game that prompts for a question and then displays either "Yes," "No," "Maybe," or "Ask again
+    later."
+     */
+
+    //Create Magic 8 ball class
+    //It shall contain an RNG
+    //It shall generate 1-4
+    //It shall output the result.
+}
