6591dff9fa7e4b846b80f964911ae77805afc426
JSCaracal <siposj@knights.ucf.edu>
2021-10-01 15:28:35 -0400

    Full program for Exercise 32

--- exercise32/src/main/java/Baseline/RandomGame.java
+++ exercise32/src/main/java/Baseline/RandomGame.java
@@ -1,38 +1,61 @@
 package Baseline;
+import java.util.InputMismatchException;
 import java.util.Random;
+import java.util.Scanner;
+
 public class RandomGame {
+    private static final Scanner input = new Scanner(System.in);
      private int difficultyLevel;
+
      public RandomGame(int difficultyLevel){
          this.difficultyLevel = difficultyLevel;
      }
      //Create a menu method that'll call games
-    void mainMenu(){
-        //Prompt user to enter difficulty
 
-        //Create loop to trap user until they have a correct input
-
-        //right input, call function
-    }
 
     //Create a function for first level
     void theGame() {
          Random rand = new Random();
-         int randomNumber;
+         boolean isGame = true;
+         int randomNumber = 0;
         //Generate random number between certain numbers depending the level
         switch (this.difficultyLevel){
             case 1:
-                randomNumber = rand.nextInt(10);
+                randomNumber = rand.nextInt(10)+1;
                 break;
             case 2:
-                randomNumber = rand.nextInt(100);
+                randomNumber = rand.nextInt(100)+1;
                 break;
             case 3:
-                randomNumber = rand.nextInt(1000);
+                randomNumber = rand.nextInt(1000)+1;
                 break;
         }
 
         //Trap user in loop until they get it right
-
+        int guess =1 ;
+        int numOfGuesses = 0;
+        System.out.println("I have my number, can you guess what it is?: ");
+        while(isGame){
+            try{
+                guess = input.nextInt();
+            }
+            catch (InputMismatchException e){
+                System.out.println("Not a numerical input");
+            }
+            if(guess == randomNumber){
+                System.out.printf("You got it in %d guesses!\n",numOfGuesses);
+                break;
+            }
+            else if(guess > randomNumber){
+                System.out.printf("Too high, guess again!: ");
+                numOfGuesses++;
+            }
+            else {
+                System.out.printf("Too low, guess again!: ");
+                numOfGuesses++;
+            }
+        }
         //prompt if its' higher or lower
+
     }
 }
--- exercise32/src/main/java/Baseline/Solution32.java
+++ exercise32/src/main/java/Baseline/Solution32.java
@@ -1,6 +1,10 @@
 package Baseline;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Solution32 {
+    private static final Scanner input = new Scanner(System.in);
     //Number game
     /*
     Create a menu that asks for the level of difficulty
@@ -11,4 +15,39 @@ public class Solution32 {
     keep track of guesses
 
      */
+    void mainMenu(){
+        //Prompt user to enter difficulty
+        int difficultyLevel = 1;
+        while (true){
+            System.out.printf("Enter the difficulty level (1, 2, or 3): ");
+            try {
+                difficultyLevel = input.nextInt();
+            }
+            catch (InputMismatchException e){
+                System.out.println("Wrong input");
+                input.next();
+                continue;
+            }
+            RandomGame game = new RandomGame(difficultyLevel);
+            game.theGame();
+            System.out.println("Would you like to play again? Y/N");
+            String choice = input.next();
+            if(choice.equals("N")){
+                break;
+            }
+        }
+
+
+
+
+
+        //Create loop to trap user until they have a correct input
+
+        //right input, call function
+    }
+
+    public static void main(String[] args) {
+        Solution32 s32 = new Solution32();
+        s32.mainMenu();
+    }
 }
