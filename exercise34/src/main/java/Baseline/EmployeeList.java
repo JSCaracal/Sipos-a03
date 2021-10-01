be46dca18e752cd148bb301ec2fae91cd4ea34cb
JSCaracal <siposj@knights.ucf.edu>
2021-10-01 17:04:20 -0400

    Initial design and psuedo

---
+++ exercise34/src/main/java/Baseline/EmployeeList.java
@@ -0,0 +1,18 @@
+package Baseline;
+
+public class EmployeeList {
+    //Initilize list of Employees, with names
+    private String names[] = new String[5];
+    public EmployeeList(){
+
+    }
+    //Method the input of employee removal
+    void removeEmployee(String emp){
+
+    }
+    //Print the list
+    void printList(){
+
+    }
+
+}
---
+++ exercise34/src/main/java/Baseline/Solution34.java
@@ -0,0 +1,15 @@
+package Baseline;
+
+import java.util.Scanner;
+
+public class Solution34 {
+    //Have user input the name for removal
+    private static final Scanner input = new Scanner(System.in);
+    String removeEmployee(){
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
+
