4c54edef357c97513a85090187a04adf6f20c8ac
JSCaracal <siposj@knights.ucf.edu>
2021-10-01 14:11:52 -0400

    Added more design logic to RandomGame.java

--- exercise32/src/main/java/Baseline/RandomGame.java
+++ exercise32/src/main/java/Baseline/RandomGame.java
@@ -7,19 +7,19 @@ public class RandomGame {
      }
      //Create a menu method that'll call games
     void mainMenu(){
+        //Prompt user to enter difficulty
 
+        //Create loop to trap user until they have a correct input
+
+        //right input, call function
     }
 
     //Create a function for first level
-    void levelOne(){
+    void theGame() {
+        //Generate random number between certain numbers depending the level
 
-    }
-    //Second level
-    void levelTwo(){
+        //Trap user in loop until they get it right
 
-    }
-    //Third
-    void LevelThree(){
-
+        //prompt if its' higher or lower
     }
 }
