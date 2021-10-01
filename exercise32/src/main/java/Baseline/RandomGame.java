c0cfeaaef4c82bc6087d2a96ea0078d07c8e6d53
JSCaracal <siposj@knights.ucf.edu>
2021-10-01 13:57:37 -0400

    Initial psuedocode

---
+++ exercise32/src/main/java/Baseline/RandomGame.java
@@ -0,0 +1,25 @@
+package Baseline;
+import java.util.Random;
+public class RandomGame {
+     private int difficultyLevel;
+     public RandomGame(int difficultyLevel){
+         this.difficultyLevel = difficultyLevel;
+     }
+     //Create a menu method that'll call games
+    void mainMenu(){
+
+    }
+
+    //Create a function for first level
+    void levelOne(){
+
+    }
+    //Second level
+    void levelTwo(){
+
+    }
+    //Third
+    void LevelThree(){
+
+    }
+}
---
+++ exercise32/src/main/java/Baseline/Solution32.java
@@ -0,0 +1,14 @@
+package Baseline;
+
+public class Solution32 {
+    //Number game
+    /*
+    Create a menu that asks for the level of difficulty
+    summon math.random
+    if player choses their level, execute the level program
+    have the player keep guessing and prompt them if their choice is too high or too low
+    display result
+    keep track of guesses
+
+     */
+}
