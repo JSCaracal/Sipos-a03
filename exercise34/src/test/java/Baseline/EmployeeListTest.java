5a20e792024673f956dae966cd613a9dd3cc67f1
JSCaracal <siposj@knights.ucf.edu>
2021-10-02 13:02:40 -0400

    Commiting exercise 34

---
+++ exercise34/src/test/java/Baseline/EmployeeListTest.java
@@ -0,0 +1,18 @@
+package Baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeListTest {
+    @Test
+    void removal_Test(){
+        String exepected[] = {"John Smith",
+                "Jackie Jackson",
+                "Amanda Cullen",
+                "Jeremy Goodwin"};
+
+        EmployeeList testObject = new EmployeeList();
+
+    }
+}
