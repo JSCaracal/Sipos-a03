010531e0f23e14f4fe5990fdee792555e3b38871
JSCaracal <siposj@knights.ucf.edu>
2021-10-03 12:55:09 -0400

    Minimal logic and psuedo code added to WinnerList Class

---
+++ exercise35/src/main/java/Baseline/WinnerList.java
@@ -0,0 +1,36 @@
+package Baseline;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class WinnerList {
+    private static Scanner input = new Scanner(System.in);
+    private ArrayList<String>winners = new ArrayList<String>();
+    //Create constructor
+    public WinnerList(){
+
+    }
+    //Have user input names via method
+    void nameEnter(){
+        //Trap the user in a while loop until blank is reached
+        while(true){
+            System.out.print("Enter a name: ");
+            if(input.next() == ""){
+                break;
+            }
+            else{
+                winners.add(input.next());
+            }
+        }
+    }
+
+    //RNG Function
+    void pickAWinner(){
+        Random rand = new Random();
+        int winningNumber = rand.nextInt(winners.size());
+        
+    }
+
+
+}
